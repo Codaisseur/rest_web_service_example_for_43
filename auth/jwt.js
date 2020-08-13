d207efc057c913360900dfaaebad9c85566874ff
Jeroen <Jeroenbruinsma@gmail.com>
2020-08-13 11:43:45 +0200

    extend the token lifetime

--- auth/jwt.js
+++ auth/jwt.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const secret = process.env.JWT_SECRET || "e9rp^&^*&@9sejg)DSUA)jpfds8394jdsfn,m";
 
 function toJWT(data) {
-  return jwt.sign(data, secret, { expiresIn: "1m" });
+  return jwt.sign(data, secret, { expiresIn: "100m" });
 }
 
 function toData(token) {
